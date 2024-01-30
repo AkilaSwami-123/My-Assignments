f0d1b093480da1acf503a35c3912a52bb393985f
AkilaSwami-123 <158035565+AkilaSwami-123@users.noreply.github.com>
2024-01-30 10:06:45 +0530

    Add files via upload
    Week1 - day 1 and day 2

---
+++ day2/FireFox.java
@@ -0,0 +1,31 @@
+package week1.day2;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear=1998;
+		char browserLogo = 'f';
+		System.out.println("The browser version is :"+ browserVersion);
+		System.out.println("The browser Name is :"+ browserName);
+		System.out.println("The Visibility  is :"+ isVisible);
+		System.out.println("The Release year is :"+ releaseYear);
+		System.out.println("The Logo is :"+ browserLogo);
+		int number = 10;
+		int number1 = 20;
+		if(number>0)
+				System.out.println("Number is positive");
+		else
+			System.out.println("Number is negative");
+			
+		if(number1%2==0)
+			System.out.println("Number is even");
+	else
+		System.out.println("Number is odd");
+		
+	}
+
+}
